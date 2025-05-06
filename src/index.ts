9b6245bb3006793c2027ef70982669f9804b7142
Vaibhav Arora <aroravaibhav817@gmail.com>
2025-05-07 03:30:26 +0530

    add index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(8000, () => {
   console.log("Server started at 8000");
